17b254f1a9cafc0cbf22b228e555bca998ea7688
Bazzan <sebake01@gmail.com>
2019-05-01 13:18:58 +0200

    CollorPads

--- BloomKeeper/Assets/Aktuella scripts/TriggerStuff/CollorPads.cs
+++ BloomKeeper/Assets/Aktuella scripts/TriggerStuff/CollorPads.cs
@@ -4,23 +4,20 @@ using UnityEngine;
 
 public class CollorPads : MonoBehaviour
 {
-    public Material material1, material2, material3;
-    private Material tempMaterial;
 
-    public GameObject pad1, pad2;
+
+    public CheckMaterialCombo checkMaterialCombo;
+    private Material tempMaterial;
+    public Material material1;
+    public Material material2;
+    public Material material3;
 
     private void Awake()
     {
-
+       GetComponentInParent<CheckMaterialCombo>();
     }
 
-    private void Update()
-    {
-        if (checkIfCorrect())
-        {
-            //Do something
-        }
-    }
+
 
     private void OnTriggerStay(Collider other)
     {
@@ -29,36 +26,39 @@ public class CollorPads : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.F))
             {
                 tempMaterial = gameObject.GetComponent<MeshRenderer>().sharedMaterial;
-                Debug.Log("press1");
-
+                //Debug.Log("press1");
                 if (tempMaterial.Equals(material1))
                 {
                     Debug.Log(tempMaterial);
-                    Debug.Log("press");
+                    //Debug.Log("press");
                     gameObject.GetComponent<Renderer>().material = material2;
                 }
                 else if (tempMaterial.Equals(material2))
                 {
                     Debug.Log(tempMaterial);
-                    Debug.Log("press");
+                    //Debug.Log("press");
                     gameObject.GetComponent<Renderer>().material = material3;
                 }
                 else if (tempMaterial.Equals(material3))
                 {
                     Debug.Log(tempMaterial);
-                    Debug.Log("press");
+                    //Debug.Log("press");
                     gameObject.GetComponent<Renderer>().material = material1;
                 }
+                Debug.Log("press");
+
+                checkMaterialCombo.checkMaterial();
+
             }
         }
     }
 
-    bool checkIfCorrect()
-    {
-        if(gameObject.GetComponent<Renderer>().material == material1 && pad1.GetComponent<Renderer>().material == material1 && pad2.GetComponent<Renderer>().material == material1)
-        {
-            return true;
-        }
-        return false;
-    }
+    //bool checkIfCorrect()
+    //{
+    //    if(gameObject.GetComponent<Renderer>().material == material1 && pad1.GetComponent<Renderer>().material == material1 && pad2.GetComponent<Renderer>().material == material1)
+    //    {
+    //        return true;
+    //    }
+    //    return false;
+    //}
 }
---
+++ BloomKeeper/Assets/CheckMaterialCombo.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckMaterialCombo : MonoBehaviour
+{
+    public GameObject TriggerObject;
+
+    public GameObject Pad1, Pad2, Pad3;
+
+    [Header("Rätt material")]
+    public Material Mat1;
+    public Material Mat2;
+    public Material Mat3;
+
+    protected Renderer ren;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        ren = GetComponent<Renderer>();
+    }
+    public void checkMaterial()
+    {
+        //Debug.Log(pad1.GetComponent<Renderer>().material.ToString() + pad2.GetComponent<Renderer>().material.ToString() + pad3.GetComponent<Renderer>().material.ToString());
+
+        Debug.Log(Pad1.GetComponent<Renderer>().sharedMaterial.ToString() + Pad2.GetComponent<Renderer>().sharedMaterial.ToString() + Pad3.GetComponent<Renderer>().sharedMaterial.ToString());
+        if(Pad1.GetComponent<Renderer>().sharedMaterial.Equals(Mat1) && Pad2.GetComponent<Renderer>().sharedMaterial.Equals(Mat2) && Pad3.GetComponent<Renderer>().sharedMaterial.Equals(Mat3))
+        {
+            TriggerObject.GetComponent<TriggeredObject>().OnTrigger();
+            Debug.Log("Colorpads Triggerd");
+        }
+        else
+        {
+            TriggerObject.GetComponent<TriggeredObject>().OnDeTrigger();
+        }
+        
+        //if(pad1.GetComponent<Renderer>().sharedMaterial)
+    }
+
+}
