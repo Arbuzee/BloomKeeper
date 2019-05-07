f25f3ef11e4ded2e74df450aa408a6b11797ec4b
alexancast <37774672+alexancast@users.noreply.github.com>
2019-05-07 12:59:46 +0200

    NPC text
    Fixed NPC text to canvas and navmesh problem for player.

--- BloomKeeper/Assets/Aktuella scripts/TriggerStuff/NPCListener.cs
+++ BloomKeeper/Assets/Aktuella scripts/TriggerStuff/NPCListener.cs
@@ -6,11 +6,6 @@ public class NPCListener : MonoBehaviour
     public AudioClip interactionAudio;
 
 
-    public void Update()
-    {
-        textBox.transform.LookAt(new Vector3(Camera.main.transform.position.x, 0, Camera.main.transform.position.z));
-    }
-
     private void OnTriggerStay(Collider other)
     {        
         if (other.CompareTag("Player") && Input.GetKeyDown(KeyCode.F)){
