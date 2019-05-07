392bcae9db5821ff437bd33f61095afdc0dfcc23
Arbuzee <37773311+Arbuzee@users.noreply.github.com>
2019-05-07 12:44:42 +0200

    Player Animations

---
+++ BloomKeeper/Assets/Scripts/PlayerAnimController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimController : MonoBehaviour
+{
+    private Animator anim;
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float left_right = Input.GetAxis("Horizontal");
+        float bwd_fwd = Input.GetAxis("Vertical");
+
+        anim.SetFloat("left_right", left_right);
+        anim.SetFloat("bwd_fwd", bwd_fwd);
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            anim.SetTrigger("decoy");
+        }
+    }
+
+    public void StandOnPad()
+    {
+        anim.SetTrigger("pressurepad");
+    }
+}
