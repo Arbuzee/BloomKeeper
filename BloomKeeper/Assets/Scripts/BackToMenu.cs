7bb99a070ea47760ff1eead759b471d001c4bd0b
Arbuzee <37773311+Arbuzee@users.noreply.github.com>
2019-05-08 16:14:31 +0200

    Bunch of fixes for playtest

---
+++ BloomKeeper/Assets/Scripts/BackToMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMenu : MonoBehaviour
+{
+    
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
