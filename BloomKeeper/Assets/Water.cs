a27ce72100ad63f338b4ed5d8ffea9f9e01cc731
alexancast <37774672+alexancast@users.noreply.github.com>
2019-05-08 10:54:28 +0200

    Water collider

---
+++ BloomKeeper/Assets/Water.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+
+    public Transform spawnPoint;
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.position = spawnPoint.position;
+        }
+    }
+
+}
