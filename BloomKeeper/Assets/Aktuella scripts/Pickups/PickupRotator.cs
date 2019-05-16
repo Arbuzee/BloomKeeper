2ce40cf8bca0e2791ad187939bb3e0240bdb651a
disaviola <37692445+disaviola@users.noreply.github.com>
2019-05-16 10:27:26 +0200

    Pickup and Canvas controller added + organizing in folders
    UI and animation scripts is now in folder under "Aktuella scripts".
    Pickup with particles added and a canvas controller for keeping count.

---
+++ BloomKeeper/Assets/Aktuella scripts/Pickups/PickupRotator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupRotator : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem onPickUpParticles;
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("collision");
+            //add to counter
+            CanvasController.Instance.addPickup();
+            ParticleSystem particleInstance = Instantiate(onPickUpParticles, transform.position, Quaternion.identity);
+            Destroy(particleInstance.gameObject, 2.5f);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ BloomKeeper/Assets/Aktuella scripts/UI and animation/CanvasController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasController : MonoBehaviour
+{
+    private int pickupAmount = 0;
+    [SerializeField] private Text pickupCounter;
+    public static CanvasController Instance;
+
+    private void Awake()
+    {
+        pickupAmount = 0;
+        pickupCounter.text = "0";
+        Instance = this;
+    }
+
+    public void addPickup()
+    {
+        pickupAmount++;
+        pickupCounter.text = pickupAmount.ToString();
+    }
+}
