38b5f03eb2a73ed92960a2c4d1079ff336cd4b55
Arbuzee <37773311+Arbuzee@users.noreply.github.com>
2019-05-16 16:50:29 +0200

    JumpPad

--- BloomKeeper/Assets/Aktuella scripts/TriggerStuff/PressurePadScripts/JumpPad.cs
+++ BloomKeeper/Assets/Aktuella scripts/TriggerStuff/PressurePadScripts/JumpPad.cs
@@ -7,6 +7,12 @@ public class JumpPad : MonoBehaviour
     public GameObject Player;
     public float JumpForce = 0;
     public ParticleSystem JumpParticle;
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -14,8 +20,15 @@ public class JumpPad : MonoBehaviour
         {
             Vector3 PadForce = new Vector3(0, JumpForce, 0);
             PlayerPhysics.Instance.PlayerVelocity += PadForce;
-            ParticleSystem instance = Instantiate(JumpParticle, transform.position, JumpParticle.transform.rotation);
-            Destroy(instance.gameObject, 3f);
+
+            //För att undvika att skapa och förstöra objekt som tar relativt mycket arbetskraft 
+            //så kan man stänga av loopen på partikel system och bara spela upp via kod när det är dags //Gleb
+            //
+            //ParticleSystem instance = Instantiate(JumpParticle, transform.position, JumpParticle.transform.rotation);
+            //Destroy(instance.gameObject, 3f);
+            JumpParticle.Play();
+
+            anim.SetTrigger("Active");
         }
 
     }
